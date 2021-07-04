bbfd6267a85dad68a2bb8564d1c551d267af494f
MuzykaAndrii <andrymyzik@gmail.com>
2021-07-05 00:06:50 +0300

    implemented bundle saving feature

--- app/t_bot.py
+++ app/t_bot.py
@@ -1,13 +1,49 @@
-from app import bot
-from app.models import User
+from app import bot, app
+from app.models import User, Bundle
 from app.helpers import manage_user
+from telebot import types
+from app import cache as storage
+from flask import url_for
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 @manage_user
 def start_command(message):
-    bot.send_message(message.chat.id, "Hello")
+    bot.send_message(message.chat.id, "Hello, i can help you to learn new vocabulary")
 
-@bot.message_handler(commands=['set'])
+@bot.message_handler(commands=['token'])
 @manage_user
-def set_bundle(message):
-    bot.send_message(message.chat.id, "Not available yet)")
+def send_token(message):
+    bot.send_message(message.chat.id, f"Your token api is: {message.chat.id}")
+
+@bot.message_handler(commands=['create_bundle'])
+@manage_user
+def create_bundle(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    end_btn = types.KeyboardButton(text='Save and exit')
+    keyboard.add(end_btn)
+
+    msg = bot.send_message(message.chat.id, "Send me word and translation in format: word - translation\nTo save all words send me '!end'", reply_markup=keyboard)
+    words = dict()
+    bot.register_next_step_handler(msg, lambda m: set_word(m, words))
+
+def set_word(message, words):
+    if message.text == 'Save and exit':
+        print(words)
+        new_bundle = Bundle(message.chat.id)
+        new_bundle.encode_words(words)
+        new_bundle.save()
+        bot.send_message(message.chat.id, "Words successfully saved, link: {}{}".format(app.config['WEBHOOK'] ,url_for('get_bundle', user_id=message.chat.id, bundle_id=new_bundle.id)), reply_markup=ReplyKeyboardRemove())
+        return
+    
+    input_data = message.text.split('-')
+    word = input_data[0].strip()
+    translation = input_data[1].strip()
+    words[word] = translation
+
+    bot.register_next_step_handler(msg, lambda m: set_word(m, words))
+
+@bot.message_handler(commands=['web'])
+@manage_user
+def get_web(message):
+
+    bot.send_message(message.chat.id, "The web application of our project")
--- app/web.py
+++ app/web.py
@@ -1,5 +1,13 @@
 from app import app
+from app.models import User, Bundle
 
 @app.route('/index')
 def index():
     return 'It works!'
+
+@app.route('/api/<int:user_id>/<int:bundle_id>')
+def get_bundle(user_id, bundle_id):
+    user = User.query.first_or_404(user_id)
+    bundle = Bundle.query.filter(Bundle.id == bundle_id, Bundle.creator_id == user_id).first_or_404()
+
+    return bundle.words
