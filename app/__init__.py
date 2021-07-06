404c90b355213d407ea8a4d23452aa612603eb87
MuzykaAndrii <andrymyzik@gmail.com>
2021-07-06 03:22:39 +0300

    some fucking changes in fucking bot

--- app/__init__.py
+++ app/__init__.py
@@ -40,5 +40,5 @@ def webhook():
 from app.t_bot import *
 from app import web
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
